bc383eca2aaa3e585030be01711c005fbe5f455c
MBugov <bugovmurillo@gmail.com>
2024-10-08 14:17:44 -0300

    ExemploFundamentos

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ExemploFundamentos.Models;
 
-
-
 // instaciando a classe
 var carro = new Carro();
 
@@ -19,3 +17,34 @@ carro1.Partida(carro1.Nome, carro1.Kilometragem);
 
 //ou
 carro1.Partida("Tucson", 85000);
+
+// ------------------- Conversao segura -------------------
+
+string variavelA = "15";
+int variavelB;
+
+if (int.TryParse(variavelA, out variavelB))
+{
+    Console.WriteLine("Conversão realizada com sucesso: " + variavelB);
+}
+else
+{
+    Console.WriteLine("Falha na conversão");
+}
+
+// ------------------- Estrutura condicional -------------------
+
+int quantidadeEmEstoque = 15;
+int quantidadeCompra = 11;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"É possivel realizar a vendas: {possivelVenda} ");
+
+if (possivelVenda)
+{
+    Console.WriteLine("Venda autorizada!");
+}
+else
+{
+     Console.WriteLine("Venda negada! \n Não temos a quantidade suficiente em estoque!");
+}
